6adb375173bbfbd23b79bfd5e3425a732d78a964
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-03-19 11:11:53 +0330

    get weather information data function created

--- script/forecast.js
+++ script/forecast.js
@@ -1,10 +1,19 @@
 const apiAccessKey = "ZYzLdD15D8yt8xipKhC9Mo8fdaCTMV9o";
 
-const getCityWeatherData = async (cityName) => {
+const getCityInformationData = async (cityName) => {
   const baseUrl = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiAccessKey}&q=${cityName}`;
   const response = await (await fetch(baseUrl)).json();
   return response[0];
 };
-getCityWeatherData("esfahan")
+// getCityInformationData("esfahan")
+//   .then((data) => console.log(data))
+//   .catch((error) => console.error(error));
+
+const getWeatherInformationData = async (id) => {
+  const baseUrl = `http://dataservice.accuweather.com/currentconditions/v1/${id}?apikey=${apiAccessKey}`;
+  const response = await (await fetch(baseUrl)).json();
+  return response;
+};
+getWeatherInformationData("1-208194_1_AL")
   .then((data) => console.log(data))
   .catch((error) => console.error(error));
