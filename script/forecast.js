9a6035fd2069f30baf64086aebb57e154ada3536
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-03-19 11:15:48 +0330

    get weather information data function created

--- script/forecast.js
+++ script/forecast.js
@@ -1,19 +1,17 @@
 const apiAccessKey = "ZYzLdD15D8yt8xipKhC9Mo8fdaCTMV9o";
 
+const getWeatherInformationData = async (id) => {
+  const baseUrl = `http://dataservice.accuweather.com/currentconditions/v1/${id}?apikey=${apiAccessKey}`;
+  const response = await (await fetch(baseUrl)).json();
+  return response[0];
+};
+
 const getCityInformationData = async (cityName) => {
   const baseUrl = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiAccessKey}&q=${cityName}`;
   const response = await (await fetch(baseUrl)).json();
   return response[0];
 };
-// getCityInformationData("esfahan")
-//   .then((data) => console.log(data))
-//   .catch((error) => console.error(error));
-
-const getWeatherInformationData = async (id) => {
-  const baseUrl = `http://dataservice.accuweather.com/currentconditions/v1/${id}?apikey=${apiAccessKey}`;
-  const response = await (await fetch(baseUrl)).json();
-  return response;
-};
-getWeatherInformationData("1-208194_1_AL")
+getCityInformationData("esfahan")
+  .then((data) => getWeatherInformationData(data.Key))
   .then((data) => console.log(data))
   .catch((error) => console.error(error));
