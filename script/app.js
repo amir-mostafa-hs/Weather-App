1acef69e6cc2e93f0596890fdb0ab682f4a1ded6
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-03-21 23:28:04 +0330

    add update UI function

--- script/app.js
+++ script/app.js
@@ -1,17 +1,35 @@
 const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+
+const updateUI = (data) => {
+  const cityDetails = data.cityDetails;
+  const weatherDetails = data.weatherDetails;
+
+  details.innerHTML = `
+    <h5 class="my-3">${cityDetails.EnglishName}</h5>
+    <div class="my-3">${weatherDetails.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weatherDetails.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>
+  `;
+
+  card.classList.contains("d-none") && card.classList.remove("d-none");
+};
 
 const updateCityCard = async (cityName) => {
   const cityDetails = await getCityInformationData(cityName);
   const weatherDetails = await getWeatherInformationData(cityDetails.Key);
 
-  return { cityDetails: cityDetails, weatherDetails: weatherDetails };
+  return { cityDetails, weatherDetails };
 };
 
-cityForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+cityForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
   const cityName = cityForm.city.value.trim();
   cityForm.reset();
   updateCityCard(cityName)
-    .then((data) => console.log(data))
+    .then((data) => updateUI(data))
     .catch((error) => console.error(error));
 });
