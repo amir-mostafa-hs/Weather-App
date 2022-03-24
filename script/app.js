0e3a4ad431f1cb1d0744055ac687ade8a4bdb3b4
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-03-24 09:29:21 +0430

    add comments

--- script/app.js
+++ script/app.js
@@ -7,24 +7,23 @@ const icon = document.querySelector(".icon img");
 // !Function For Update UI Data
 const updateUI = (data) => {
   const { cityDetails, weatherDetails } = data;
-
+  // !Details Element Update
   details.innerHTML = `
     <h5 class="my-3">${cityDetails.EnglishName}</h5>
     <div class="my-3">${weatherDetails.WeatherText}</div>
     <div class="display-4 my-4">
       <span>${weatherDetails.Temperature.Metric.Value}</span>
       <span>&deg;C</span>
-    </div>
-  `;
-
+    </div>`;
+  // !Show Weather Icon
   const iconSrc = `./images/icons/${weatherDetails.WeatherIcon}.svg`;
   icon.setAttribute("src", iconSrc);
-
+  // !Show Status Of The Day
   const timeSrc = weatherDetails.IsDayTime
     ? "./images/day.svg"
     : "./images/night.svg";
   time.setAttribute("src", timeSrc);
-
+  // !Show Card
   card.classList.contains("d-none") && card.classList.remove("d-none");
 };
 // !Get Data From forecast.js File
@@ -35,8 +34,9 @@ const getDataUpdateCityCard = async (cityName) => {
 };
 // !Add Submit Event Listener To Form
 cityForm.addEventListener("submit", (evt) => {
-  // !Disable Default
+  // !Disable Default Behavior
   evt.preventDefault();
+  // !Get Input Value
   const cityName = cityForm.city.value.trim();
   cityForm.reset();
   getDataUpdateCityCard(cityName)
